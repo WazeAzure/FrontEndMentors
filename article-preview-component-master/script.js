b51a04a4cd1fc500765b7c35250628d46143d4a2
WazeAzure <55005873+WazeAzure@users.noreply.github.com>
2020-08-27 16:45:12 +0800

    Update script.js

--- article-preview-component-master/script.js
+++ article-preview-component-master/script.js
@@ -1,8 +1,14 @@
 $(function(){
 
-  $('svg').click(function(){
-    $('.popup').show();
-    $('.right-down-icon-popup').show();
-    $('.triangle-down').show();
+  $('.share').click(function(){
+    $('svg').removeClass();
+    $('.icons').show();
+    $('path').css('fill','white');
+    $('.svg').css('background-color','hsl(217, 19%, 35%)');
+    $('.svg').css('border-radius','50%');
   })
+//  $('html').not($('.share')).click(function(){
+//    $('.icons').hide();
+//  });
+
 });
